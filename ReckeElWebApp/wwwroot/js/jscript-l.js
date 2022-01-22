69b9aa325b1c7e89c4d17323f109139057684e97
Teis Draiby <teis@teis.net>
2022-01-22 11:11:08 +0100

    Format JavaScript HTML text strings

--- ReckeElWebApp/wwwroot/js/jscript-l.js
+++ ReckeElWebApp/wwwroot/js/jscript-l.js
@@ -78,7 +78,53 @@
 			$("#test2").css("position", "fixed");
 		}
 
-		$('#MenuBackgroundLeftInner').html('<a id="el_installation" class="menuLeft" href="elinstallation.html"><img  src="/images/elinstallation.jpg" class="produkt-image"><h5 class="menu-text">El Installation</h5><img  src="/images/mand.png" class="pil-right"></a><a id="Landbrug" class="menuLeft" href="landbrug.html"><img  src="/images/Landbrug.jpg" class="produkt-image"><h5 class="menu-text">Landbrug</h5><img  src="/images/mand.png" class="pil-right"</a><a id="Industri" class="menuLeft" href="industri.html"><img  src="/images/industri.jpg" class="produkt-image"><h5 class="menu-text">Industri</h5><img  src="/images/mand.png" class="pil-right"></a><a id="belysning" class="menuLeft" href="belysning.html"><img  src="/images/belysning.jpg" class="produkt-image"><h5 class="menu-text">Belysning</h5><img  src="/images/mand.png" class="pil-right"></a><a id="Energioptimering" class="menuLeft" href="energioptimering.html"><img  src="/images/energioptimering.jpg" class="produkt-image"><h5 class="menu-text">Energioptimering</h5><img  src="/images/mand.png" class="pil-right"></a><a id="Eleftersyn" class="menuLeft" href="eleftersyn.html"><img  src="/images/elsyn.jpg" class="produkt-image"><h5 class="menu-text">Eleftersyn</h5><img  src="/images/mand.png" class="pil-right"></a><a id="it_data" class="menuLeft" href="it-data.html"><img  src="/images/it-data.jpg" class="produkt-image"><h5 class="menu-text">Tele IT & Netværk</h5><img  src="/images/mand.png" class="pil-right"></a><a id="hvidevarer" class="menuLeft" href="hvidevarer.html"><img  src="/images/hvidevarer.jpg" class="produkt-image"><h5 class="menu-text">Hvidevarer</h5><img  src="/images/mand.png" class="pil-right"></a><a id="butik" class="menuLeft" href="butik.html"><img  src="/images/butik.jpg" class="produkt-image"><h5 class="menu-text">Butik</h5><img  src="/images/mand.png" class="pil-right"> </a>');
+		$('#MenuBackgroundLeftInner').html(`
+			<a id="el_installation" class="menuLeft" href="elinstallation.html">
+				<img src="/images/elinstallation.jpg" class="produkt-image">
+				<h5 class="menu-text">El Installation</h5>
+				<img src="/images/mand.png" class="pil-right">
+			</a>
+			<a id="Landbrug" class="menuLeft" href="landbrug.html">
+				<img src="/images/Landbrug.jpg" class="produkt-image">
+				<h5 class="menu-text">Landbrug</h5>
+				<img src="/images/mand.png" class="pil-right">
+			</a>
+			<a id="Industri" class="menuLeft" href="industri.html">
+				<img src="/images/industri.jpg" class="produkt-image">
+				<h5 class="menu-text">Industri</h5>
+				<img src="/images/mand.png" class="pil-right">
+			</a>
+			<a id="belysning" class="menuLeft" href="belysning.html">
+				<img src="/images/belysning.jpg" class="produkt-image">
+				<h5 class="menu-text">Belysning</h5>
+				<img src="/images/mand.png" class="pil-right">
+			</a>
+			<a id="Energioptimering" class="menuLeft" href="energioptimering.html">
+				<img src="/images/energioptimering.jpg" class="produkt-image">
+				<h5 class="menu-text">Energioptimering</h5>
+				<img src="/images/mand.png" class="pil-right">
+			</a>
+			<a id="Eleftersyn" class="menuLeft" href="eleftersyn.html">
+				<img src="/images/elsyn.jpg" class="produkt-image">
+				<h5 class="menu-text">Eleftersyn</h5>
+				<img src="/images/mand.png" class="pil-right">
+			</a>
+			<a id="it_data" class="menuLeft" href="it-data.html">
+				<img src="/images/it-data.jpg" class="produkt-image">
+				<h5 class="menu-text">Tele IT & Netværk</h5>
+				<img src="/images/mand.png" class="pil-right">
+			</a>
+			<a id="hvidevarer" class="menuLeft" href="hvidevarer.html">
+				<img src="/images/hvidevarer.jpg" class="produkt-image">
+				<h5 class="menu-text">Hvidevarer</h5>
+				<img src="/images/mand.png" class="pil-right">
+			</a>
+			<a id="butik" class="menuLeft" href="butik.html">
+				<img src="/images/butik.jpg" class="produkt-image">
+				<h5 class="menu-text">Butik</h5>
+				<img src="/images/mand.png" class="pil-right">
+			</a>
+		`);
 
 		menuSelected();
 
@@ -102,8 +148,59 @@
 
 		if (Width < minWidth - 475) {
 			$("#MenuBackgroundLeftInner").hide('slow');
-			$('#menu2').html(' <br/><br/><br/><div class="eight columns "><a id="el_installation" class="menuLeft" href="elinstallation.html"><img  src="/images/elinstallation.jpg" class="produkt-image"><h5 class="menu-text">El Installation</h5><img  src="/images/mand.png" class="pil-right"></a><a id="Landbrug" class="menuLeft" href="landbrug.html"><img  src="/images/Landbrug.jpg" class="produkt-image"><h5 class="menu-text">Landbrug</h5><img  src="/images/mand.png" class="pil-right"</a><a id="Industri" class="menuLeft" href="industri.html"><img  src="/images/industri.jpg" class="produkt-image"><h5 class="menu-text">Industri</h5><img  src="/images/mand.png" class="pil-right"></a><a id="hvidevarer" class="menuLeft" href="hvidevarer.html"><img  src="/images/hvidevarer.jpg" class="produkt-image"><h5 class="menu-text">Hvidevarer</h5><img  src="/images/mand.png" class="pil-right"></a><a id="butik" class="menuLeft" href="butik.html"><img  src="/images/butik.jpg" class="produkt-image"><h5 class="menu-text">Butik</h5><img  src="/images/mand.png" class="pil-right"> </a></div>' +
-				'<div class="eight columns "><a id="Energioptimering" class="menuLeft" href="energioptimering.html"><img  src="/images/energioptimering.jpg" class="produkt-image"><h5 class="menu-text">Energioptimering</h5><img  src="/images/mand.png" class="pil-right"></a><a id="Eleftersyn" class="menuLeft" href="eleftersyn.html"><img  src="/images/elsyn.jpg" class="produkt-image"><h5 class="menu-text">Eleftersyn</h5><img  src="/images/mand.png" class="pil-right"></a><a id="belysning" class="menuLeft" href="belysning.html"><img  src="/images/belysning.jpg" class="produkt-image"><h5 class="menu-text">Belysning</h5><img  src="/images/mand.png" class="pil-right"></a><a id="it_data" class="menuLeft" href="it-data.html"><img  src="/images/it-data.jpg" class="produkt-image"><h5 class="menu-text">Tele IT & Netværk</h5><img  src="/images/mand.png" class="pil-right"></a></div><div class="clear"></div>');
+			$('#menu2').html(`
+				<br /><br /><br />
+				<div class="eight columns">
+					<a id="el_installation" class="menuLeft" href="elinstallation.html">
+						<img src="/images/elinstallation.jpg" class="produkt-image">
+						<h5 class="menu-text">El Installation</h5>
+						<img src="/images/mand.png" class="pil-right">
+					</a>
+					<a id="Landbrug" class="menuLeft" href="landbrug.html">
+						<img src="/images/Landbrug.jpg" class="produkt-image">
+						<h5 class="menu-text">Landbrug</h5>
+						<img src="/images/mand.png" class="pil-right">
+					</a>
+					<a id="Industri" class="menuLeft" href="industri.html">
+						<img src="/images/industri.jpg" class="produkt-image">
+						<h5 class="menu-text">Industri</h5>
+						<img src="/images/mand.png" class="pil-right">
+					</a>
+					<a id="hvidevarer" class="menuLeft" href="hvidevarer.html">
+						<img src="/images/hvidevarer.jpg" class="produkt-image">
+						<h5 class="menu-text">Hvidevarer</h5>
+						<img src="/images/mand.png" class="pil-right">
+					</a>
+					<a id="butik" class="menuLeft" href="butik.html">
+						<img src="/images/butik.jpg" class="produkt-image">
+						<h5 class="menu-text">Butik</h5>
+						<img src="/images/mand.png" class="pil-right">
+					</a>
+				</div>
+				<div class="eight columns">
+					<a id="Energioptimering" class="menuLeft" href="energioptimering.html">
+						<img src="/images/energioptimering.jpg" class="produkt-image">
+						<h5 class="menu-text">Energioptimering</h5>
+						<img src="/images/mand.png" class="pil-right">
+					</a>
+					<a id="Eleftersyn" class="menuLeft" href="eleftersyn.html">
+						<img src="/images/elsyn.jpg" class="produkt-image">
+						<h5 class="menu-text">Eleftersyn</h5>
+						<img src="/images/mand.png" class="pil-right">
+					</a>
+					<a id="belysning" class="menuLeft" href="belysning.html">
+						<img src="/images/belysning.jpg" class="produkt-image">
+						<h5 class="menu-text">Belysning</h5>
+						<img src="/images/mand.png" class="pil-right">
+					</a>
+					<a id="it_data" class="menuLeft" href="it-data.html">
+						<img src="/images/it-data.jpg" class="produkt-image">
+						<h5 class="menu-text">Tele IT & Netværk</h5>
+						<img src="/images/mand.png" class="pil-right">
+					</a>
+				</div>
+				<div class="clear"></div>
+			`);
 
 			$(".pil-right").css("width", "21px");
 			$(".menuLeft").css("width", "340px");
@@ -146,14 +243,43 @@
 			var LocalUrl = location.href;
 			// $("#MenuBackground").hide();
 			// $('#Menu').css("width", Width + "px");
-			$('#kontakt').html('Recke-El ApS  <a href="tel:62 26 10 18">62 26 10 18</a>  <a href="mailto:mail@recke-el.dk">mail@recke-el.dk</a> ');
-			$('#test2').html('<nav class="nav-main"><a id="kontakt-os" href="kontakt.html">Kontakt</a><a> &#183; </a><a id="led" href="led.html">Ekspert i Led belysning</a><a> &#183; </a><a> &#183; </a><a id="om-os" href="om.html">Om os</a><a> &#183; </a><a href="/">Forside</a></nav>');
+			$('#kontakt').html('Recke-El ApS<a href="tel:62 26 10 18">62 26 10 18</a> <a href="mailto:mail@recke-el.dk">mail@recke-el.dk</a>');
+			$('#test2').html(`
+				<nav class="nav-main">
+					<a id="kontakt-os" href="kontakt.html">Kontakt</a>
+					<a> &#183; </a>
+					<a id="led" href="led.html">Ekspert i Led belysning</a>
+					<a> &#183; </a>
+					<a> &#183; </a>
+					<a id="om-os" href="om.html">Om os</a>
+					<a> &#183; </a>
+					<a href="/">Forside</a>
+				</nav>
+			`);
 			menuSelected();
 		}
 
 		if (Width > (MinMenuWidth)) {
-			$('#kontakt').html(' <ul><li>Recke-El ApS</li><li>Stationsvej 54</li><li>5771 Stenstrup</li><li>62 26 10 18</li><li><a href="mailto:mail@recke-el.dk">mail@recke-el.dk</a></li></ul>');
-			$('#test2').html('<nav class="nav-main"><a id="kontakt-os" href="kontakt.html">Kontakt</a><a> &#183; </a><a id="led" href="led.html">Ekspert i Led belysning</a><a> &#183; </a><a id="om-os" href="om.html">Om os</a><a> &#183; </a><a href="/">Forside</a></nav>');
+			$('#kontakt').html(`
+				<ul>
+					<li>Recke-El ApS</li>
+					<li>Stationsvej 54</li>
+					<li>5771 Stenstrup</li>
+					<li>62 26 10 18</li>
+					<li><a href="mailo:mail@recke-el.dk">mail@recke-el.dk</a></li>
+				</ul>
+			`);
+			$('#test2').html(`
+				<nav class="nav-main">
+					<a id="kontakt-os" href="kontakt.html">Kontakt</a>
+					<a> &#183; </a>
+					<a id="led" href="led.html">Ekspert i Led belysning</a>
+					<a> &#183; </a>
+					<a id="om-os" href="om.html">Om os</a>
+					<a> &#183; </a>
+					<a href="/">Forside</a>
+				</nav>
+			`);
 			menuSelected();
 			$('.nav-main').css('margin-top', '0px');
 		}
