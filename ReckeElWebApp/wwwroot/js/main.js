b0d16048703090c0693b3e01c0a0d9a715a139a3
Teis Draiby <teis@teis.net>
2022-01-25 16:16:42 +0100

    Clean up main page JavaScript

--- ReckeElWebApp/wwwroot/js/main.js
+++ ReckeElWebApp/wwwroot/js/main.js
@@ -1,41 +1,31 @@
-﻿imageresize(); // Triggers when document first loads
-// $("html, body").animate({ scrollTop: 310 }, 4000);
-$(window).bind("resize", function() { // Adjusts image when browser resized
-	imageresize();
-});
+﻿let minWidth = 1060;
+let mobileWidth = 860;
 
-function imageresize() {
-	var minWidth = 1060;
-	var LmobilWidth = 860;
-	var SmobilWidth = 660;
-	var Width = 1000;
-	var Height = 1000;
+resizePage();
 
-	if (typeof window.innerWidth != 'undefined') {
-		Width = window.innerWidth;
-		Height = window.innerHeight;
-	}
-	else if (typeof document.documentElement != 'undefined' && typeof document.documentElement.clientWidth != 'undefined' && document.documentElement.clientWidth != 0) {
-		Width = document.documentElement.offsetWidth;
-		Height = document.documentElement.offsetHeight;
-	}
+$(window).resize(resizePage);
 
-	if (Width < minWidth) {
-		$(".topText").hide('slow');
+function resizePage() {
+	// https://stackoverflow.com/questions/6942785/window-innerwidth-vs-document-documentelement-clientwidth
+
+	let width = window.innerWidth; // document.documentElement.offsetWidth;
+
+	if (width < minWidth) {
+		$(".topText").hide();
 		$(".topLogo").css("width", "100%");
-		$("#topLogo").css("width", Width + "px");
+		$("#topLogo").css("width", width + "px");
 	}
 	else {
-		$(".topText").show('slow');
+		$(".topText").show();
 		$(".topLogo").css("width", "70%");
 	}
 
-	if (Width < LmobilWidth) {
-		$(".topLogo").css('position', "absolute");
-		$(".header").css('position', "absolute");
+	if (width < mobileWidth) {
+		$(".topLogo").css("position", "absolute");
+		$(".header").css("position", "absolute");
 	}
 	else {
-		$(".topLogo").css('position', "fixed");
-		$(".header").css('position', "fixed");
+		$(".topLogo").css("position", "fixed");
+		$(".header").css("position", "fixed");
 	}
 };
