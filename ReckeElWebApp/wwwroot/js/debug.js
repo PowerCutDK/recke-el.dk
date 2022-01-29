5bebd553bd87397b523202aad961fb2609015ec5
Teis Draiby <teis@teis.net>
2022-01-29 13:29:28 +0100

    Add element outlines display feature

---
+++ ReckeElWebApp/wwwroot/js/debug.js
@@ -0,0 +1,18 @@
+﻿let gridIsOn = false;
+
+function toggleGrid() {
+	if (!gridIsOn) {
+		$("body").addClass("debug-grid");
+		gridIsOn = true;
+	}
+	else {
+		$("body").removeClass("debug-grid");
+		gridIsOn = false;
+	}
+}
+
+window.addEventListener("keydown", event => {
+	if (event.key.toLowerCase() === "x") {
+		toggleGrid();
+	}
+});
