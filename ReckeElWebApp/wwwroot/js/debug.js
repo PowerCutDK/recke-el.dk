cffe3f80287b5f3212ab928c435cddb2e1eec973
Teis Draiby <teis@teis.net>
2022-02-02 13:34:52 +0100

    Add debug mouseover color feature

--- ReckeElWebApp/wwwroot/js/debug.js
+++ ReckeElWebApp/wwwroot/js/debug.js
@@ -1,18 +1,44 @@
 ﻿let gridIsOn = false;
+let hoverElement;
+//let debugOverlay = document.createElement('div');
+//debugOverlay.id = "debug-overlay";
 
-function toggleGrid() {
+function toggleDebug() {
 	if (!gridIsOn) {
-		$("body").addClass("debug-grid");
+		$("body").addClass("debug-enabled");
 		gridIsOn = true;
+		//document.body.appendChild(debugOverlay);
+		window.addEventListener("mouseover", onMouseOver);
+
+		// Get the current element under the mouse
+		let mouseOverElements = document.querySelectorAll(":hover");
+		let mouseOverElement = mouseOverElements[mouseOverElements.length - 1];
+		onMouseOver({ target: mouseOverElement });
 	}
 	else {
-		$("body").removeClass("debug-grid");
+		$("body").removeClass("debug-enabled");
 		gridIsOn = false;
-	}
+		//debugOverlay.remove();
+		window.removeEventListener("mouseover", onMouseOver);	}
 }
 
 window.addEventListener("keydown", event => {
 	if (event.key.toLowerCase() === "x") {
-		toggleGrid();
+		toggleDebug();
 	}
 });
+
+function onMouseOver(event) {
+	hoverElement?.classList.remove("debug-hover");
+	hoverElement = event.target;
+	const selectorString = getSelectorString(hoverElement);
+	console.log(selectorString);
+	hoverElement.classList.add("debug-hover");
+}
+
+function getSelectorString(element) {
+	const tag = element.tagName.toLowerCase();
+	const id = element.id ? "#" + element.id : "";
+	const classes = element.className ? "." + element.className.replace(" ", ".") : "";
+	return tag + id + classes;
+}
